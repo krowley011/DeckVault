c6eecda8a21c51e009f8ad01c764ae0ea49cf585
krowley011 <KRStevens1@student.fullsail.edu>
2023-10-14 18:48:23 -0500

    Implemented navigation bar

--- app/src/main/java/com/example/deckvault/HomePage.kt
+++ app/src/main/java/com/example/deckvault/HomePage.kt
@@ -2,10 +2,53 @@ package com.example.deckvault
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomePage : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        //Setting on click listener for bottom navigation bar
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnClickListener {menuItem ->
+            //Selected home button
+            when(menuItem.id) {
+                R.id.home_bn -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+            }
+            //Selected decks button
+            when(menuItem.id){
+                R.id.decks_bn -> {
+                    replaceFragment(DeckFragment())
+                    true
+                }
+            }
+            //Selected camera button
+            when(menuItem.id){
+                R.id.camera_bn -> {
+                    replaceFragment(CameraFragment())
+                    true
+                }
+            }
+            //Selected settings button
+            when(menuItem.id){
+                R.id.settings_bn -> {
+                    replaceFragment(SettingsFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit()
     }
 }
--- app/src/main/java/com/example/deckvault/MainActivity.kt
+++ app/src/main/java/com/example/deckvault/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.deckvault
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        buttonLogin = findViewById(R.id.login_btn)
+        buttonLogin.setOnClickListener {
+            val i = Intent(this, HomePage::class.java)
+            startActivity(i)
+        }
     }
 }
